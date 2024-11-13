b80129cea65bc6cd082464aff06e698dceef3ccf
datvct <vancongthanhdata10@gmail.com>
2024-11-13 23:52:15 +0700

    feat: git init

---
+++ src/main/java/fit/iuh/edu/vn/vancongthanhdat_21117961_btl/VanCongThanhDat21117961BtlApplication.java
@@ -0,0 +1,13 @@
+package fit.iuh.edu.vn.vancongthanhdat_21117961_btl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanCongThanhDat21117961BtlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VanCongThanhDat21117961BtlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fit/iuh/edu/vn/vancongthanhdat_21117961_btl/VanCongThanhDat21117961BtlApplicationTests.java
@@ -0,0 +1,13 @@
+package fit.iuh.edu.vn.vancongthanhdat_21117961_btl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VanCongThanhDat21117961BtlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
